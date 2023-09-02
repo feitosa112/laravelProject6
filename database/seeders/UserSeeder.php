d21adb6099e8ceada3c7f47aebd0e725027b3ec9
Feitosa Filip <112kuzmanovic@gmail.com>
2023-09-02 19:56:04 +0200

    *Dodao sam UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use Faker\Factory as FakerFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker =FakerFactory::create();
+       for($i = 0; $i < 500; $i++){
+        User::create([
+            'name'=>$faker->name,
+            'email'=>$faker->email,
+            'password'=>Hash::make('12345678')
+
+        ]);
+       }
+    }
+}
